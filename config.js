e998f33cbeb79e78e723663b4817bb91d61c4d5d
risendev <86288779+risenjs@users.noreply.github.com>
2021-09-12 15:00:48 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const ext = {
+  tokens: ['token1', 'token2'],//tek token istiyorsanız token2 yazan yeri silin
+  mentionPrefix: false,
+  prefixes: [{ prefix: '!', space: false }, { prefix: '-', space: false }]
+};
+
+
+
+
+
+module.exports = ext;
